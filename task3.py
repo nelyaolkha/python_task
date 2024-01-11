e3c29811f2cb074a070d603eaf8da1ec0ae75001
nelly <nelyaolkha@gmail.com>
2024-01-11 17:44:59 +0200

    not yet

--- task3.py
+++ task3.py
@@ -1,6 +1,5 @@
 Word = input("Введіть слово:")
 print(Word)
 Довжина = len(Word)
-print("парні букви у слові"):
+print("парні букви у слові")
 for i in range(0,Довжина -1,2):
-    print (print("index[", i, "]", word[i]))
